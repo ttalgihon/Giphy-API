912ed21d6030aac5feefb3ba4f12d2ba62489c68
ttalgihon <vivithefool@gmail.com>
2019-01-13 03:35:46 -0500

    Final commit

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
             btncreated.text(animalArray[i]);
             $(".buttonsArea").append(btncreated);
         }
-    }
+    };makingButtons()
 
     //Puts the search bar text into the array.
     $("#submitButton").on("click", function(event){
@@ -22,61 +22,59 @@ $(document).ready(function(){
         // The value from the search bar is put into the array.
         animalArray.push(inputAnimal);
         makingButtons();
-    })
+    });
     
     /* The click-listener will watch to see which button the user clicks...
     and clear the previous GIFs from the page to display the new GIF picked by the user */
-    $(".animal-btn").click(function(){
-
+    $(".animal-btn").on("click", function(){
         // This empties the webpage of any previous GIFS beign displayed
-        $(".giphyDivImages").empty();
+        $("div.col-sm-8").empty();
 
         // Acessing the GIPHY API.
-        var attrToObtain = $(this).attr("value");
+        // "$(this) refers to the button that was clicked
+        var qValue = $(this).attr('value');
         var APIkey = '5dEk32QTWRRL9CGmYQg3Ucg8xBWIDfxI';
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ attrToObtain +"&api_key="+ APIkey +"&limit=10";
-        console.log("this-2", this);
-        console.log("obtainvariable-3", attrToObtain);
-        
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + qValue + "&api_key=" + APIkey + "&limit=10";
+
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(x){
-            $(".giphyDivImages").text(JSON.stringify(X));
-            /// Storing an array of results in the 'result' var
+        })
+        // After the data comes back from the API
+        .then(function(x){
+            // Storing an array of results in the 'result' var
             var result = x.data;
-
-            console.log("qurl-4", queryURL);
-            console.log("meta-5", x);
 
             // Looping over every result item
             for (var u = 0; u < result.length; u++) {
-                // Creating a div for the gif images
-                var anDIV = $("<div class=\"animalItem\"></div>")
-                // grabs the rating of the image [PG , PG-13 , etc..]
-                var rating = result[u].rating;
-                var p = $("<p>").text("Rating: " + rating);
 
-                var animated = result[u].images.fixed_height.url;
-                var still = result[u].images.fixed_height_still.url;
+                // Only take action in the gif don't have an R rating.
+                if (result[u].rating !== "r"){
 
+                    // Creating a div for the gif images
+                    var anDIV = $("<div>")
 
-                var anIMG = $("<img>");
-                anIMG.attr("src", still);
-                anIMG.attr("data-still:", still);
-                anIMG.attr("data-animate", animated)
-                anIMG.attr("data-state", still);
-                anIMG.addClass("gifImage");
-                
-                anDIV.append(p);
-                anDIV.append(anIMG);
-                
-                $(".giphyDivImages").prepend(anDIV);
+                    // Grabs the rating of the image [PG , PG-13 , etc..]
+                    var rating = result[u].rating;
+
+                    var pa = $("<p>").text("Rating: " + rating);
+    
+                    var animated = result[u].images.fixed_height.url;
+                    var still = result[u].images.fixed_height_still.url;
+    
+                    var anIMG = $("<img>");
+                    anIMG.attr("src", animated);
+                    anIMG.attr("data-still:", still);
+                    anIMG.attr("data-animate", animated)
+                    anIMG.attr("data-state", still);
+                    anIMG.addClass("gifImage");
+                    
+                    var oo = anDIV.append(pa);
+                    anDIV.append(anIMG);
+                    
+                    $("div.col-sm-8").append(anDIV);
+                }
             }
-        })
-    })
-    makingButtons()
+        });
+    });  
 }); 
-
-
-
